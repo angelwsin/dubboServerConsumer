95fca4e23749cc988825c185f55c81b14108ea0d
angelwsin <angelwsin@Lenovo-PC>
2016-01-11 15:26:00 +0800

    dobbo comsumer

---
+++ dubboServerConsumer/src/main/java/com/dubbo/controller/DubboTestController.java
@@ -0,0 +1,10 @@
+package com.dubbo.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class DubboTestController {
+	
+	
+
+}
---
+++ dubboServerConsumer/src/test/java/com/dubbo/test/DubboTest.java
@@ -0,0 +1,28 @@
+package com.dubbo.test;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.znn.provider.DemoService;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath*:applicationContext-*.xml")
+@Configuration("classpath*:spring-servlet.xml")
+public class DubboTest {
+      
+	@Autowired
+	  private  DemoService demoService;
+	 @Autowired
+     private ApplicationContext applicationContext;
+	@Test
+	public void   say(){
+		DemoService  s = 	(DemoService) applicationContext.getBean("demoService");
+		System.out.println(s.sayHello("world--"));
+		System.out.println(demoService.sayHello("world"));
+	}
+}
